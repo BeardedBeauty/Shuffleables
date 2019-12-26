8643c4ebbaea82086d0c88b720911a2a87f57191
John Ochs <john.w.ochs0@gmail.com>
2019-12-26 11:23:59 -0600

    added shake effect

--- src/App.js
+++ src/App.js
@@ -5,68 +5,79 @@ import Nav from "./components/Nav";
 import Cheesebox from "./components/Cheese";
 
 class App extends React.Component {
-  state = {
-    cheese: cheese,
-    score: 0,
-    clicc: [],
-    geese: ""
-  }
+    state = {
+        cheese: cheese,
+        score: 0,
+        clicc: [],
+        geese: "",
+        imageclass: ["imgholder z-depth-3 shakeme", "imgholder z-depth-3"],
+        i: 1
+    }
 
-  handleClick = id => {
-    const y = this.state.clicc;
-    // const o = y.find(i => i === id);
-    if (!y.find(i => i === id)) {
-      y.push(id);
-      this.setState({
-        score: this.state.score + 1,
-        clicc: y,
-        geese: "Correct"
-      });
-    } else {
-      this.setState(
-        {
-          score: this.state.score = 0,
-          clicc: [],
-          geese: "Wrong!"
+    handleClick = id => {
+        const y = this.state.clicc;
+        // const o = y.find(i => i === id);
+        if (!y.find(i => i === id)) {
+            y.push(id);
+            this.setState({
+                score: this.state.score + 1,
+                clicc: y,
+                geese: "Correct"
+            });
         }
-      );
+        else {
+            this.setState(
+                {
+                    score: this.state.score = 0,
+                    clicc: [],
+                    geese: "Wrong!",
+                    i: 0
+                }
+            );
+            setTimeout(this.stop, 700);
+        };
+        console.log(this.state.clicc)
+        this.shuffle();
     };
-    console.log(this.state.clicc)
-    this.shuffle();
-  };
 
-  shuffle = () => {
-    let t = this.state.cheese;
-    for (let e = t.length - 1; e > 0; e--) {
-      let r = Math.floor(Math.random() * (e + 1));
-      [t[e], t[r]] = [t[r], t[e]];
+    shuffle = () => {
+        let t = this.state.cheese;
+        for (let e = t.length - 1; e > 0; e--) {
+            let r = Math.floor(Math.random() * (e + 1));
+            [t[e], t[r]] = [t[r], t[e]];
+        };
+        this.setState({ cheese: t })
     };
-    this.setState({ cheese: t })
-  };
 
-  render() {
-    return (
-      <div>
-        <Nav
-          score={this.state.score}
-          guess={this.state.geese}
-        />
-        <br /> <br />
-        <div className="container">
-          <div className="row">
-            {this.state.cheese.map(slice =>
-              <Cheesebox
-                image={slice.img}
-                id={slice.id}
-                handleClick={this.handleClick}
-                key={slice.id}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  };
+    stop = () => {
+        this.setState({ i: 1 });
+        console.log("this stupid timer was definitely the hardest part");
+    }
+
+    render() {
+        return (
+            <div>
+                <Nav
+                    score={this.state.score}
+                    guess={this.state.geese}
+                />
+                <br /> <br />
+                <div className="container">
+                    <div className="row">
+                        {this.state.cheese.map(slice =>
+                            <Cheesebox
+                                imageclass={this.state.imageclass[this.state.i]}
+                                image={slice.img}
+                                id={slice.id}
+                                handleClick={this.handleClick}
+                                key={slice.id}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        );
+    };
 };
 
 export default App;
--- src/components/Cheese/index.js
+++ src/components/Cheese/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Cheese(props) {
     return (
         <div className="holder col s6 m6 l3">
-            <div className="imgholder z-depth-3" onClick={() => props.handleClick(props.id)}>
+            <div className={props.imageclass} onClick={() => props.handleClick(props.id)}>
                 <img className="cliccimg" id={props.id} src={props.image} />
             </div>
         </div>
